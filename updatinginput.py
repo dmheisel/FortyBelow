5afaa6399c330f49f34851503cc19018588c737d
David Heisel <47954555+dmheisel@users.noreply.github.com>
2019-03-29 14:23:31 -0500

    Update updatinginput.py

--- updatinginput.py
+++ updatinginput.py
@@ -9,7 +9,7 @@ Take the top discarded card
 
 def input_validator(range_ = None, acceptable_chars = None):
     """
-    Decorator function intended to verify user input.  Checks if input is an integer within range passed as arg to decorator with keyword 'range_'
+    Decorator function used to validate user input.  If range is specified, checks if input is an int within the range.  Otherwise will check if input is contained within list of acceptable characters
     """
     def validating(func):
         def validated(*args, **kwargs):
@@ -33,9 +33,10 @@ def input_validator(range_ = None, acceptable_chars = None):
     return validating
 
 
+
 flip = "Flip a card in your hand."
 draw = "Draw a card from the deck."
-take_discard = "Take the top card on the discard pile."
+take_discard = "Take the top card from the the discard pile."
 check_score = "Check the scoresheet and this hand's current score."
 choose_position = (
     "Please select a position in your hand from the chart below.\n"
@@ -48,21 +49,24 @@ action_key = {0: check_score, 1: draw, 2: take_discard, 3: flip}
 
 @input_validator(range_ = range(4))
 def regular_input(self, player):
-    prompt = f"{player.name}, it is your turn.  You may:\n"
+    prompt = f"You may:\n"
     for number, action in action_key.items():
         prompt += f'{str(number)}. {action} \n'
-    index = int(input(prompt))
-    while action_key[index] == check_score:
-        print(f"{player}'s current score is {self.tally_score(player)}")
-        index = int(input(prompt))
-    return index
+    return input(prompt)
 
 @input_validator(range_ = range(2))
 def final_input(self, player):
-    prompt = f"{player.name}, it is the last turn in the round.  You may:\n"
+    prompt = f"It is the last turn in the round.  You may:\n"
     prompt += f'0. {action_key[0]} \n'
-    prompt += f'1. {action_key[1]}' 
-    return int(input(prompt))
+    prompt += f'1. {action_key[1]}'
+    #index = int(input(prompt))
+    #while action_key[index] == check_score:
+    #    print(
+    #        f"{player}'s current hand is worth #{self.tally_score(player)}\n"
+   #         f"The scoreboard is: {self.scoresheet}"
+   #         )
+   #     index = int(input(prompt))
+    return input(prompt)
     
 @input_validator(range_ = range(1,7))
 def get_card_position():
@@ -73,9 +77,9 @@ def keep_or_discard(card):
     return input(f"You drew a {card}. \nDo you wish to keep it?\nInput 'y' for Yes, 'n' for No\n")
 
 def starting_two_flips(player):
-    print(f'{player}, flip your first card.\n')
+    print(f'{player}, flip your first card.')
     pos_1 = get_card_position()-1
-    print(f'{player}, flip your second card.\n')
+    print(f'{player}, flip your second card.')
     pos_2 = get_card_position()-1
     while pos_2 == pos_1:
         print('You have already chosen that card: \n')
@@ -86,16 +90,38 @@ def starting_two_flips(player):
     player.hand.show()
 
 def turn(self, player, is_last =  False):
+    print(
+        f'The discard pile currently shows {self.discard_pile.card_list[0]}.\n'
+        f'{player}, your hand is:'
+        )
+    player.hand.show()
+
     if is_last:
         action = action_key[final_input(self, player)]
+        while action == check_score:
+            print(
+            f"{player}'s current hand is worth {self.tally_score(player)}\n"
+            f"The scoreboard is: {self.scoresheet}"
+            )
+            action = action_key[final_input(self, player)]
     else:
         action = action_key[regular_input(self, player)]
+        while action == check_score:
+            print(
+            f"{player}'s current hand is worth {self.tally_score(player)}\n"
+            f"The scoreboard is: {self.scoresheet}"
+            )
+            action = action_key[regular_input(self, player)]
+
 
     if action == flip:
-        pos = get_card_position()-1
-        player.hand.cards[pos].flip()
-        player.hand.show()
-        
+        flipped_card = player.hand.cards[get_card_position()-1]
+        while not flipped_card.hidden:
+            print("That card is already revealed.")
+            flipped_card = player.hand.cards[get_card_position()-1]
+        flipped_card.flip()
+        print(f'{player} revealed {flipped_card}.  Their hand is now:')
+        player.hand.show() 
         
     if action == draw:
         drawn_card = [card.flip() for card in self.deck.draw()][0]
@@ -120,4 +146,11 @@ def turn(self, player, is_last =  False):
         print(f"{player} took a {new_card} from the discard pile into their hand.")
         self.discard_pile.discard(old_card)
         print(f"{player}'s hand is now:")
+        player.hand.show()
+
+    if is_last:
+        for card in player.hand.cards:
+            card.flip()
+        print("All cards are now revealed. \n"
+               "Your hand is now:")
         player.hand.show()
